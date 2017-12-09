4db4c94dc71dd0c5b98c6abe60a9520b5c75029f
Danilo Cândido <danilod07@gmail.com>
2017-12-08 21:07:24 -0300

    skipping authorize in some controllers

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,30 +1,23 @@
 class LineItemsController < ApplicationController
   include CurrentCart
+  
+  skip_before_action :authorize, only: :create
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /line_items
-  # GET /line_items.json
   def index
     @line_items = LineItem.all
   end
 
-  # GET /line_items/1
-  # GET /line_items/1.json
-  def show
-  end
+  def show; end
 
-  # GET /line_items/new
   def new
     @line_item = LineItem.new
   end
 
-  # GET /line_items/1/edit
   def edit
   end
 
-  # POST /line_items
-  # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
@@ -40,8 +33,6 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
@@ -54,8 +45,6 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
   def destroy
     @line_item.destroy
     respond_to do |format|
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,8 @@
 class StoreController < ApplicationController
-  skip_before_action :authorize
-
   include CurrentCart
   
+  skip_before_action :authorize
+
   before_action :set_cart
   def index
     @products = Product.order(:title)
